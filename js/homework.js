20969740d6c1ee8f05390b8ba7a9df48452b695a
SaleemiHaseebullah2001 <73466496+SaleemiHaseebullah2001@users.noreply.github.com>
2022-05-09 11:14:44 +0200

    Update homework.js

--- js/homework.js
+++ js/homework.js
@@ -4,19 +4,19 @@ function $get(id)
 }
 function LOWERCASE()
 {
-    var strInputText =$get("txt1").value;
+    var strInputText = $get('txt1').value;
     strInputText = strInputText.toLowerCase();
-    $get("txt1").value = strInputText;
+    $get('txt1').value = strInputText;
 }
 function UPPERCASE()
 {
-	var strInputText 	=  $get( "txt1" ).value;
+	var strInputText =  $get( "txt1" ).value;
 	strInputText = strInputText.toUpperCase();
 	$get('txt1').value = strInputText;
 }
 function MIRRORED()
 {
-    var strInputText 	=  $get( "txt1" ).value;
+    var strInputText =  $get( "txt1" ).value;
     strInputText = strInputText.split("").reverse().join("");
     $get('txt1').value = strInputText;
 }
