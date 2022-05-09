20222d5437df30ec8d6fb144a412b4886b035596
SaleemiHaseebullah2001 <73466496+SaleemiHaseebullah2001@users.noreply.github.com>
2022-05-09 11:13:03 +0200

    code improvement
    Added Lowercase Function and relative button.

--- js/homework.js
+++ js/homework.js
@@ -2,11 +2,16 @@ function $get(id)
 {
     return document.getElementById(id);
 }
+function LOWERCASE()
+{
+    var strInputText =$get("txt1").value;
+    strInputText = strInputText.toLowerCase();
+    $get("txt1").value = strInputText;
+}
 function UPPERCASE()
 {
 	var strInputText 	=  $get( "txt1" ).value;
 	strInputText = strInputText.toUpperCase();
-
 	$get('txt1').value = strInputText;
 }
 function MIRRORED()
